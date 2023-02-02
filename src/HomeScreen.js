defc1241e4b5608b70a0be89db46440026e39377
Cleo Gao <celeo127@outlook.com>
2023-02-02 19:44:59 +0800

    first commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,16 @@
+import './HomeScreen.css';
+import NavBar from './NavBar';
+const HomeScreen = () => {
+    return (
+        <div className='homeScreen'>
+            {/* NavBar */}
+            <NavBar />
+            
+            {/* Banner */}
+
+            {/* Rwos */}
+        </div>
+    );
+}
+
+export default HomeScreen;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCtoNcRPO5fqdzJ-Hm1JFofer_1NWs6B5Y",
+    authDomain: "netflix-clone-d3863.firebaseapp.com",
+    projectId: "netflix-clone-d3863",
+    storageBucket: "netflix-clone-d3863.appspot.com",
+    messagingSenderId: "542352545184",
+    appId: "1:542352545184:web:7fe5d4e5e379ffbe7a00c5"
+  };
