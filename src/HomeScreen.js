bee867757fc52de55ee94a5d26d36649530a3f66
Cleo Gao <celeo127@outlook.com>
2023-02-06 20:27:08 +0800

    adding categories

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,6 +1,8 @@
 import Banner from './Banner';
 import './HomeScreen.css';
 import NavBar from './NavBar';
+import requests from './Request';
+import Row from './Row';
 const HomeScreen = () => {
     return (
         <div className='homeScreen'>
@@ -10,7 +12,19 @@ const HomeScreen = () => {
             {/* Banner */}
             <Banner />
 
-            {/* Rwos */}
+            {/* Rows */}
+            <Row 
+                title='NETFLIX ORIGINALS'
+                fetchUrl={requests.fetchNetflixOriginals}
+                isLargeRow
+            />
+            <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+            <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+            <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+            <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+            <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+            <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} />
+            <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
         </div>
     );
 }
